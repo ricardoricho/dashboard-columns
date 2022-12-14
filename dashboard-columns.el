123b749a4abd1d4f30b5fec88eba9d46f69fd012
Ricardo Arredondo <ricardo.richo@gmail.com>
2022-12-14 22:58:54 +0100

    Add license

--- dashboard-columns.el
+++ dashboard-columns.el
@@ -1,5 +1,27 @@
 ;;; dashboard-columns.el --- Columns for emacs dashboard.   -*- lexical-binding: t -*-
+;; Copyright (C) 2022  emacs-dashboard maintainers
+
+;; Author: Ricardo Arredondo
+;; URL: https://github.com/ricardoricho/dashboard-columns
+;; Version: 0.1.0
+;; Keywords: tools
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 ;;; Commentary:
+;; Add columns to emacs-dashboard
 ;;; Code:
 
 (require  'cl-lib)
